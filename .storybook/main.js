45a78eb7d7959d4b959057c21b8d63cdd44ba639
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2020-04-23 16:02:55 +0200

    Configure webpack to splitChunks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,21 @@
+const merge = require('webpack-merge');
+
+const maxAssetSize = 1024 * 1024;
+
 module.exports = {
+  webpackFinal: (config) =>
+    merge(config, {
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          minSize: 30 * 1024,
+          maxSize: maxAssetSize,
+        },
+      },
+      performance: {
+        maxAssetSize: maxAssetSize,
+      },
+    }),
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
