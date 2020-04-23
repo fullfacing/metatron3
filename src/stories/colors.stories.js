8999e83fa0563452fb3c3dc55795ffff2f824539
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2020-04-23 14:19:36 +0200

    Add Foundation docs

--- src/stories/colors.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ColorPalette } from '@theme-ui/style-guide';
-
-export default {
-  title: 'Components | Colors',
-};
-
-export const Palette = () => <ColorPalette />;
